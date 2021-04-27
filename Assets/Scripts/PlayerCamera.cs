06a22b097a9f29304aa6a6117ee839e958aae343
ItsJordeh <ItsJordeh@gmail.com>
2021-04-27 12:57:28 -0600

    InitialSetup
    Added Player Movement and camera attached to player

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float rotationSpeed = 5f;
+    float offset;
+
+    public float minTurnAngle = -90.0f;
+    public float maxTurnAngle = 0f;
+    private float rotX;
+    
+    
+    public bool invertMouse, autoLockCursor;
+
+    private Camera playerCamera;
+
+    void Start()
+    {
+        offset = Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    void Awake()
+    {
+        playerCamera = this.gameObject.GetComponent<Camera>();
+        Cursor.lockState = (autoLockCursor)?CursorLockMode.Locked:CursorLockMode.None;
+    }
+
+    void LateUpdate()
+    {
+
+        
+
+        float horizontal = Input.GetAxis("Mouse X") * rotationSpeed;
+        float vertical = Input.GetAxis("Mouse Y") * rotationSpeed;
+       
+        player.transform.Rotate(new Vector3( 0, horizontal, 0), Space.Self);
+        transform.RotateAround(player.transform.position, player.transform.right, -vertical);
+       
+        // float y = Input.GetAxis("Mouse X") * rotationSpeed;
+        // rotX += Input.GetAxis("Mouse Y") * rotationSpeed;
+        // player.transform.Rotate(0, rotX, 0);
+
+        // rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+        // transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
+        // transform.position = player.transform.position - (transform.forward * offset);
+
+
+        
+        // player.transform.Rotate(0, horizontal, 0);
+        // float desiredYAngle = player.transform.eulerAngles.y;
+        
+        // Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        // transform.position = player.transform.position - (rotation * offset);
+        // transform.LookAt(player.transform);
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // float speed = (moveSpeed + (Input.GetAxis("Fire3") * shiftAdditionalSpeed));
+        // this.gameObject.transform.Translate(Vector3.forward * speed * Input.GetAxis("Vertical"));
+        // this.gameObject.transform.Translate(Vector3.right * speed * Input.GetAxis("Horizontal"));
+        // this.gameObject.transform.Translate(Vector3.up * speed * (Input.GetAxis("Jump") + Input.GetAxis("Fire1") * -1));
+        // this.gameObject.transform.Rotate(Input.GetAxis("Mouse Y") * sensitivity * ((invertMouse) ? 1 : -1), Input.GetAxis("Mouse X") * sensitivity *((invertMouse) ? -1 : 1), 0);
+        // this.gameObject.transform.localEulerAngles = new Vector3(this.gameObject.transform.localEulerAngles.x,this.gameObject.transform.localEulerAngles.y, 0);
+
+
+        if(Cursor.lockState == CursorLockMode.None && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (Cursor.lockState == CursorLockMode.Locked && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed = 20f;
+    public float inputHMove, inputVMove ;
+
+    public bool isJumping, isRunning;
+    
+    public Vector3 movement;
+
+    Vector3 x, z;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+        
+    }
+    void FixedUpdate()
+    {
+        ProcessMovement();
+    }
+
+
+    //Process users inputs for translation to movement
+    void ProcessInput()
+    {
+        inputHMove = Input.GetAxis("Horizontal");
+        inputVMove = Input.GetAxis("Vertical");
+
+        x = transform.right * inputHMove;
+        z = transform.forward * inputVMove;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+        }
+        else isJumping = false;
+
+
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        else isRunning = false;
+
+
+    }
+    
+    void ProcessMovement()
+    {
+        // movementX = inputHMove * 20f * Time.deltaTime;
+        // movementY = inputVMove * 20f * Time.deltaTime;
+
+        // if(movementX <= 0f)
+        // {
+        //     movementX = 0;
+        // }
+
+        movement = (x + z).normalized * speed;
+
+        rb.AddForce(movement);
+
+
+    }
+
+
+
+
+}
+    
+  
